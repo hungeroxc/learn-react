ffb6a6bbf51ae159e819f143dc0f502ce4984238
oxc <oxc@oxc.com>
2018-06-04 06:39:04 +0800

    up

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import Counter from './components/Counter.jsx'
-import reducer from './reducers/reducers'
-import {createStore} from 'redux'
+import { render } from 'react-dom'
 
-const store = createStore(reducer)
-
-const render = () => ReactDOM.render(
-    <Counter 
-        value={store.getState()}
-        increment={() => store.dispatch({type: 'INCREMENT'})}
-        decrement={() => store.dispatch({type: 'DECREMENT'})}
-    />,
+render(
+    <div>123</div>,
     document.querySelector('#app')
 )
-render()
 
-store.subscribe(render)
+
+
+
